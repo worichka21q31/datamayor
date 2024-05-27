9e3990679c62ef3d4fd212aa656f375388815599
worichka21q31 <160734161+worichka21q31@users.noreply.github.com>
2024-05-27 17:28:48 +0500

    Update datamayor.cpp

--- datamayor.cpp
+++ datamayor.cpp
@@ -1,64 +1,49 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 #include <string>
 #include <string.h>
 #include <cstdlib>
-#include <fstream>
-#include <Windows.h>
+
 using namespace std;
 
+double Plus(double a, double b)
+{
+    return a + b;
+}
+double Minus(double a, double b)
+{
+    return a - b;
+}
+double Delit(double a, double b)
+{
+    return a / b;
+}
+double Umn(double a, double b)
+{
+    return a * b;
+}
+double Stepen(double a, double b)
+{
+    return pow(a, b);
+}
+double Coren(double a)
+{
+    return sqrt(a);
+}
+void MakeDes(double (*funs)())
+{
+    cout << funs();
+}
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	srand(time(NULL));
+    double a;
+    double b;
 
-	/*
-		ofstream fout;
-		fout.open("myFile.txt", ofstream::app);
+    cout << "Введите числа\n";
+    cin >> a;
+    cin >> b;
+    cout << "Выберите метод\n";
 
-		if (!fout.is_open())
-		{
-			cerr << "errrerrerrerere";
-		}
-		else
-		{
-			string name;
-			cin.ignore(3200, '\n');
-			for (int i = 0; i < 4; i++)
-			{
-				cout << "Введите Имя И Фамилию " << i+1 << "\n";
-				getline(cin, name, '\n');
-				fout  << name << "\n";
-			}
+    MakeDes(Delit());
 
-		}
-
-
-		fout.close();
-	*/
-	ofstream fout;
-	fout.open("myFile.txt", ofstream::app);
-	ifstream fin;
-	fin.open("myFile.txt");
-
-	if (!fin.is_open())
-	{
-		cerr << "errrerrerrerere";
-	}
-	else
-	{
-		cout << "Файл Открыт \n";
-		char sym;
-		string name;
-
-		while (!fin.eof())
-		{
-			fin >> name;
-			cout << name << "\n";
-		}
-	}
-	fin.close();
-
-	return 0;
 }
